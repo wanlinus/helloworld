121ea58b144c0bef147440e1bd95e98769eb9b4b
Linus Taylor <wanlinus@qq.com>
2018-09-28 17:25:16 +0800

    feature:添加Controller

--- src/main/java/cn/wanlinus/helloworld/HelloworldApplication.java
+++ src/main/java/cn/wanlinus/helloworld/HelloworldApplication.java
@@ -2,11 +2,12 @@ package cn.wanlinus.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class HelloworldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
 }
---
+++ src/main/java/cn/wanlinus/helloworld/HomeController.java
@@ -0,0 +1,16 @@
+package cn.wanlinus.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wanli
+ * @date 2018-09-28
+ */
+@RestController
+public class HomeController {
+    @GetMapping("")
+    public String sad() {
+        return "OK";
+    }
+}
