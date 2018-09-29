2081124325fdbc636ab5b240606b8cc8ca3a9400
Linus Taylor <wanlinus@qq.com>
2018-09-29 09:54:09 +0800

    2018年09月29日09:54:03

--- src/main/java/cn/wanlinus/helloworld/HomeController.java
+++ src/main/java/cn/wanlinus/helloworld/HomeController.java
@@ -1,5 +1,7 @@
 package cn.wanlinus.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("")
     public String sad() {
         return "OK";
     }
+
     @GetMapping("a")
-    public String dd(){
+    public String dd() {
+        logger.info("a");
         return "a OKdsad";
     }
 }
